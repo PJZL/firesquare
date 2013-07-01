2f543938fc0457ab4484f204b3a54675abba86f5
Paweł Preczyński <pawel@boost.no>
2013-07-01 11:03:37 +0200

    Added 'express' server for browser testing. Added icon.

---
+++ server.js
@@ -0,0 +1,15 @@
+(function(){
+  var express = require('express')
+    , app = express();
+
+  app.configure(function() {
+    var hourMs = 1000*60*60;
+    app.use(express.static(__dirname, { maxAge: hourMs }));
+    app.use(express.directory(__dirname));
+    app.use(express.errorHandler());
+  });
+
+  app.listen(3000);
+
+  console.log('running!');
+}());
