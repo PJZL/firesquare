14d1884f296fa66dbd77aa169e6a1b85af83678f
Paweł Preczyński <pawel@boost.no>
2013-07-04 15:49:52 +0200

    Added refresh option to recent checkins.

--- src/js/view/recent.js
+++ src/js/view/recent.js
@@ -14,13 +14,48 @@ define([
     _drawer.setTitle('Recent checkins');
     _drawer.setContent(_.template(template));
 
-    recent = new Recent();
-    recent.on('add', _add);
-    _fetch = recent.fetch();
+    _recent = new Recent();
+    _recent.on('add', _add);
+    _fetch = _recent.fetch();
+
+    $('.update').on('click', _update);
+  }
+
+  function _update(){
+
+    function _success(){
+      $('p[role="status"]').hide('fast');
+    }
+
+    function _error(){
+      var _text = $('p[role="status"] > span').text();
+      $('p[role="status"] > span').text('Update failed!');
+      $('p[role="status"]').hide('slow', function(){
+         $('p[role="status"] > span').text(_text);
+      });
+    }
+
+    $('div[role="main"]').scrollTop(0);
+    $('p[role="status"]').show('fast', function(){
+      if (_fetch !== undefined &&
+          typeof(_fetch.abort) === 'function' ) {
+        _fetch.abort();
+      }
+      _fetch = _recent.fetch({
+        success: _success,
+        error: _error
+      });
+    });
+    
   }
 
   function _add(checkin) {
-    $('.recent').append(_.template(checkinTemplate, checkin));
+    if (_recent.models.length > 0 &&
+        parseInt(checkin.get('createdAt'), 10) < parseInt(_recent.models[_recent.models.length-1].get('createdAt'), 10)) {
+      $('.recent').prepend(_.template(checkinTemplate, checkin));
+    } else {
+      $('.recent').append(_.template(checkinTemplate, checkin));
+    }
   }
 
   function _remove () {
@@ -28,7 +63,7 @@ define([
         typeof(_fetch.abort) === 'function' ) {
       _fetch.abort();
     }
-    recent.off('add', _add);
+    _recent.off('add', _add);
   }
 
   return Backbone.View.extend({
